04c983ef1dcbcd90f883a6ad0c2a7b8674f14834
azizurr.bappy <azizurr.bappy@gmail.com>
2026-09-14 01:44:11 +0600

    fix: update getOriginalUrl options to use 'returnDocument' instead of 'new'

--- src/controllers/url.controllers.ts
+++ src/controllers/url.controllers.ts
@@ -70,7 +70,7 @@ export async function getOriginalUrl(req: Request, res: Response) {
       { slug },
       { $inc: { clicks: 1 } },
       {
-        new: true,
+        returnDocument: 'after',
       },
     );
 
