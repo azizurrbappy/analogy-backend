2c4a4fbfd365307ec6dc870533faf19e1eec61cd
azizurr.bappy <azizurr.bappy@gmail.com>
2026-09-14 01:12:48 +0600

    feat: enhance database connection handling with existing connection check and improved error logging

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,11 +3,18 @@ import config from './config.js';
 
 async function connectDB() {
   try {
+    if (mongoose.connection.readyState === 1) {
+      console.log('[database]: Already connected');
+      return;
+    }
+
     await mongoose.connect(config.MONGO_URI);
 
     console.log('[database]: Database connected successfully');
   } catch (err) {
-    console.log('[database error]: Database connection error');
+    console.error('[database error]: Database connection error', err);
+
+    throw err;
   }
 }
 
