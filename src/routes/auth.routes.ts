f48d68c8250b0d9898a8d44eef0453be50022619
azizurr.bappy <azizurr.bappy@gmail.com>
2026-09-15 12:26:50 +0600

    refactor: remove unused authentication routes and clean up auth router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,38 +4,8 @@ import * as authControllers from '../controllers/auth.controllers.js';
 const authRouter = Router();
 
 /**
- * POST /api/auth/signup
- */
-authRouter.post('/registration', authControllers.registration);
-
-/**
  * POST /api/auth/check-username
  */
 authRouter.post('/check-username', authControllers.checkUsername);
-
-/**
- * POST /api/auth/login
- */
-authRouter.post('/login', authControllers.login);
-
-/**
- * get /api/auth/get-me
- */
-authRouter.get('/get-me', authControllers.getMe);
-
-/**
- * get /api/auth/refresh-token
- */
-authRouter.get('/refresh-token', authControllers.refreshToken);
-
-/**
- * get /api/auth/logout
- */
-authRouter.get('/logout', authControllers.logout);
-
-/**
- * get /api/auth/logout-all
- */
-authRouter.get('/logout-all', authControllers.logoutAll);
 
 export default authRouter;
